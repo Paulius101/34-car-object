6f952fea0a82edec25e26eb7f9811190d18a84ac
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-07-20 20:42:31 +0300

    initial commit

---
+++ Car.js
@@ -0,0 +1,14 @@
+class Car {
+    constructor(data) {
+        this.data = data;
+    }
+
+    intro() {
+        console.log(`This is ${this.data.brand}.`);
+    }
+
+
+}
+
+module.exports = Car;
+
